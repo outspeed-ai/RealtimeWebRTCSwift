f8aaf025e72ef15bafd46e7f367de1adef74a40f
pallavag <agpallav@gmail.com>
2025-01-03 21:47:57 -0800

    Initial Commit

---
+++ VoiceModeWebRTCSwift/VoiceModeWebRTCSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  VoiceModeWebRTCSwiftApp.swift
+//  VoiceModeWebRTCSwift
+//
+//  Created by Pallav Agarwal on 1/3/25.
+//
+
+import SwiftUI
+
+@main
+struct VoiceModeWebRTCSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
